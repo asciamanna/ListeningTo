c65bd985dd6fef86add5dc09382ed3ed0504cede
Anthony Sciamanna <asciamanna@gmail.com>
2014-04-26 12:24:23 -0400

    adding missing controller.

---
+++ ListeningTo/Controllers/TopArtistsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LastfmClient.Responses;
+using LastfmClient;
+using ListeningTo.Repositories;
+
+namespace ListeningTo.Controllers
+{
+    public class TopArtistsController : ApiController
+    {
+      public IEnumerable<LastfmUserTopArtist> GetTopArtists([FromUri] int count = 25) {
+        return new LastfmUserRepository().FindTopArtists(count);
+      }
+    }
+}
