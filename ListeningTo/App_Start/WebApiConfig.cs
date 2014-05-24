ad42366c4f0dccbb8972242b0b3396d99c90f760
Anthony Sciamanna <asciamanna@gmail.com>
2014-05-24 18:48:41 -0400

    adding jsonp formatter

--- ListeningTo/App_Start/WebApiConfig.cs
+++ ListeningTo/App_Start/WebApiConfig.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
 using System.Web.Http;
+using WebApiContrib.Formatting.Jsonp;
 
 namespace ListeningTo {
   public static class WebApiConfig {
     public static void Register(HttpConfiguration config) {
       // Web API configuration and services
       config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+      config.Formatters.Insert(0, new JsonpMediaTypeFormatter(new JsonMediaTypeFormatter()));
       // Web API routes
       config.MapHttpAttributeRoutes();
 
