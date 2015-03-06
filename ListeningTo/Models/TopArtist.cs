dc8bf95847f231c486b878d9c4f86fdcff4b5fb8
Anthony Sciamanna <asciamanna@gmail.com>
2015-03-05 19:28:58 -0500

    refactored TopArtist model object.

--- ListeningTo/Models/TopArtist.cs
+++ ListeningTo/Models/TopArtist.cs
@@ -12,18 +12,16 @@ namespace ListeningTo.Models {
     public string ArtistImageLocation { get; set; }
 
     public static IEnumerable<TopArtist> FromRepositoryObjects(IEnumerable<LastfmUserTopArtist> lastfmTopArtists) {
-      var topArtists = new List<TopArtist>();
-      foreach (var artist in lastfmTopArtists) {
-        topArtists.Add(
-           new TopArtist {
-             ArtistImageLocation = artist.LargeImageLocation,
-             Name = artist.Name,
-             Rank = artist.Rank,
-             PlayCount = artist.PlayCount,
-           }
-        );
-      }
-      return topArtists;
+      return lastfmTopArtists.Select(CreateTopArtist);
+    }
+
+    private static TopArtist CreateTopArtist(LastfmUserTopArtist artist) {
+      return new TopArtist {
+        ArtistImageLocation = artist.LargeImageLocation,
+        Name = artist.Name,
+        Rank = artist.Rank,
+        PlayCount = artist.PlayCount,
+      };
     }
   }
 }
